3dba71b9f7a33ca3da806dc15dfadcec8faa2717
GURUCHARAN-PROG <gurucharancj3@gmail.com>
2026-01-29 13:37:41 +0530

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-# Import models to make them available to the package
-from .user import User
-from .prediction import Prediction
-from .company_profile import CompanyProfile
